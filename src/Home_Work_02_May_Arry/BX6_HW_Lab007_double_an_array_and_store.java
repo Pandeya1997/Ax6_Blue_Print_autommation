945f5f6924a01bdb3e5b22db978bacfca1d5360c
Pandeya1997 <abhishekpy55@gmail.com>
2024-05-03 23:25:05 +0530

    doubple array print

---
+++ src/Home_Work_02_May_Arry/BX6_HW_Lab007_double_an_array_and_store.java
@@ -0,0 +1,15 @@
+package Home_Work_02_May_Arry;
+
+public class BX6_HW_Lab007_double_an_array_and_store {
+    public static void main(String[] args) {
+        int [] arr01 = {1,2,3};
+        for (int i = 0; i<arr01.length; i++){
+            System.out.print(arr01[i]+ " ");
+        }
+        System.out.print("\n");
+        int [] arr02 = {2,4,6};
+        for (int j = 0; j<arr02.length; j++){
+            System.out.print(arr02[j] + " ");
+        }
+    }
+}
--- src/Home_Work_02_May_Arry/BX6_HW_Lab06_Print_All_Marks.java
+++ src/Home_Work_02_May_Arry/BX6_HW_Lab06_Print_All_Marks.java
@@ -1,6 +1,6 @@
 package Home_Work_02_May_Arry;
 //Create a Program which takes arrays and print all the marks.
-public class BX6_HW_Lab05_Print_All_Marks {
+public class BX6_HW_Lab06_Print_All_Marks {
     public static void main(String[] args) {
          int  [] marks= {40,50,60,90,70,100,99};
          for(int i = 0; i< marks.length ; i++){
