add4f9a351a4e3f4ca7ed73a694425898a3a3522
Pandeya1997 <abhishekpy55@gmail.com>
2024-05-07 22:45:12 +0530

    swich precties works code bx6 class hw double_an_array_and_store --> Program update

--- src/Home_Work_02_May_Arry/BX6_HW_Lab007_double_an_array_and_store.java
+++ src/Home_Work_02_May_Arry/BX6_HW_Lab007_double_an_array_and_store.java
@@ -2,14 +2,16 @@ package Home_Work_02_May_Arry;
 
 public class BX6_HW_Lab007_double_an_array_and_store {
     public static void main(String[] args) {
-        int [] arr01 = {1,2,3};
-        for (int i = 0; i<arr01.length; i++){
-            System.out.print(arr01[i]+ " ");
+        int[] arrays = {5, 7, 10};
+        int[] arrays2 = new int[3];
+        for (int i = 0; i < arrays.length; i++) {
+            arrays2[i] = arrays[i] * 2;
         }
-        System.out.print("\n");
-        int [] arr02 = {2,4,6};
-        for (int j = 0; j<arr02.length; j++){
-            System.out.print(arr02[j] + " ");
+        System.out.print("Printing Arrays!!  -->" + " ");
+
+        for (int i = 0; i < arrays2.length; i++) {
+            System.out.print(arrays2[i] + " ");
+
         }
     }
 }
