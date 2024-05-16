63339e53a6f2f9230f0013a135b7590761192007
Pandeya1997 <abhishekpy55@gmail.com>
2024-05-16 23:22:56 +0530

    Java_method_to_transpose_a_given_2D_array

---
+++ src/Home_work_04_May_2D_Array/Java_method_to_transpose_a_given_2D_array.java
@@ -0,0 +1,37 @@
+package Home_work_04_May_2D_Array;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class Java_method_to_transpose_a_given_2D_array {
+    /*
+    Write a Java method to transpose a given 2D array.
+{{1,4,7},
+{2,5,8},
+{3,6,9}}
+ {{1,2,3},
+ {4,5,6},
+ {7,8,9}}
+     */
+   // static final int N = 3;
+    public static void main(String[] args) {
+        int A [][] = {{1,4,7}, {2,5,8}, {3,6,9}};
+        int B[][] = new int[3][3] , i, j;
+
+
+        for ( i = 0; i<A.length ; i++) {
+           // System.out.print(i +", ");
+            int k =0;
+            int m = 0;
+            for ( j = 0 ; j<A[i].length; j++){
+             //  System.out.print(A[i][j]);
+               B[i][j] =A[j][i];
+               System.out.print(B[i][j]);
+            }
+            System.out.print("\n");
+
+
+
+        }
+
+    }
+}
