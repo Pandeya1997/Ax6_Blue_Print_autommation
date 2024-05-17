9d4fa5bfaa194429c4b24589094269bb690b9553
Pandeya1997 <abhishekpy55@gmail.com>
2024-05-17 08:27:15 +0530

    oops class and how

---
+++ src/BX6_CP_OOPS_11_MAY/LAB_163.java
@@ -0,0 +1,11 @@
+package BX6_CP_OOPS_11_MAY;
+
+public class LAB_163 {
+    public static void main(String[] args) {
+        Dog german = new Dog(); // A,b
+        german.name = "Seru";
+        System.out.println(german.name);
+        Dog lab = new Dog(); // a,B
+        Dog pitbull = new Dog(); //A,B
+    }
+}
