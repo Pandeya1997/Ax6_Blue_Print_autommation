dc9bfc9cf35b74fa1ffa8c4ccd1a9ec9b43e27a4
Pandeya1997 <abhishekpy55@gmail.com>
2024-05-31 08:18:51 +0530

    25 may code push

---
+++ src/BX6_25_May/collections/Lab215.java
@@ -0,0 +1,33 @@
+package BX6_25_May.collections;
+
+public class Lab215 {
+    public static void main(String[] args) {
+        Day d = Day.FRIDAY;
+        System.out.println(d);
+
+
+        for (Size size : Size.values()) {
+            System.out.println("Size: " + size + ", Abbreviation: " + size.getSize());
+        }
+    }
+}
+enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
+
+enum Size {
+    SMALL("S"), MEDIUM("M"), LARGE("L"), EXTRA_LARGE("XL");
+    private String size;
+
+    Size(String s) {
+        this.size = s;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
