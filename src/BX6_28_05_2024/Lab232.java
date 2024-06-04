3351b08a8620ac565aba10a7a42ea0c955fef76f
abhishek pandey <abhishekpy55@gmail.com>
2024-06-04 19:08:27 +0530

    code push

--- src/BX6_28_05_2024/Lab232.java
+++ src/BX6_28_05_2024/Lab232.java
@@ -1,4 +1,5 @@
 package BX6_28_05_2024;
+import java.sql.SQLOutput;
 import java.util.List;
 import java.util.ArrayList;
 public class Lab232 {
@@ -19,5 +20,40 @@ public class Lab232 {
          mylist.add("Abhishek"); // Duplicate Allowed
         System.out.println(mylist);
         System.out.println(mylist.size());
+
+        mylist.remove("Abhishek");
+        System.out.println(mylist);
+        System.out.println(mylist.size());
+        mylist.clear();
+        System.out.println(mylist);
+        System.out.println(mylist.size());
+        System.out.println(mylist.isEmpty());
+        System.out.println("----------------");
+        mylist.add("Abhishek ");
+        mylist.add("Abhay");
+        mylist.add("pravesh");
+        mylist.add("sweta");
+        mylist.add("Abhishek");
+        mylist.add("Abhishek"); // Duplicate Allowed
+        System.out.println(mylist);
+        //System.out.println(mylist.size());
+        mylist.set(1,"Abhay kumar Ydev");
+        System.out.println(mylist);
+        mylist.add("pandey");
+        mylist.add(844817);
+        mylist.add(true);
+
+        System.out.println(mylist);
+    // How to print ?
+    //  [Abhishek , Abhay kumar Ydev, pravesh, sweta, Abhishek, Abhishek, pandey, 844817, true ]
+
+        for (int i = 0; i<mylist.size() ; i++) {
+            System.out.println(mylist.get(i));
+        }
+        System.out.println("??????????????????????????????????");
+        // For each loop
+        for (Object obj : mylist){
+           System.out.println(obj);
+        }
     }
 }
---
+++ src/BX6_28_05_2024/Lab_233.java
@@ -0,0 +1,15 @@
+package BX6_28_05_2024;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab_233 {
+    public static void main(String[] args) {
+        List mylist = new ArrayList();
+        mylist.add("Abhishek");
+        mylist.add("Anuska");
+        mylist.add(12345);
+        mylist.add(true);
+        System.out.println(mylist);
+    }
+}
