5bd74f82d06d99553d684175a7efb114adf0411f
Pandeya1997 <abhishekpy55@gmail.com>
2024-06-09 23:52:15 +0530

    Hasp Map

--- src/Jun/Lab254.java
+++ src/Jun/Lab254.java
@@ -1,7 +1,37 @@
 package Jun;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 public class Lab254 {
     public static void main(String[] args) {
+ Map <String, String> map = new HashMap();
+ map.put("Frist Name", "Abhishek");
+ map.put("Last Name", "Pandey");
+ map.put("email", "abhishekpy55@gmail.com");
+ map.put("phone", "8448172617");
+ map.put("City", "Noida");
+map.put("state", "Utter Pradesh");
+map.put("Country", "India");
+map.put("Zip","201301");
+//System.out.println(map);
+
+Map<String,String> Map1 = new HashMap();
+Map1.put("Frist Name", "Amit");
+Map1.put("Last Name", "Pandey");
+Map1.put("email", "Amit@gmail.com");
+Map1.put("phone", "9452636502");
+Map1.put("City", "Noida");
+Map1.put("state", "Utter Pradesh");
+Map1.put("Country", "India");
+Map1.put("Zip", "201301");
+//System.out.println(Map1);
+//System.out.println(map.equals(Map1));
+ArrayList list = new ArrayList();
+list.add(map);
+list.add(Map1);
+System.out.println(list);
+System.out.println(list.get(0));
+System.out.println(list.get(1));
 
     }
 }
---
+++ src/Jun/Lab256.java
@@ -0,0 +1,17 @@
+package Jun;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab256 {
+    public static void main(String[] args) {
+        // Generics
+        // Generics is a feature in Java that was introduced in Java 5.0.
+        // It allows you to abstract over types.
+
+        List mylist = new ArrayList();
+        mylist.add("Pramod");
+        mylist.add(123);
+        mylist.add(true);
+    }
+}
