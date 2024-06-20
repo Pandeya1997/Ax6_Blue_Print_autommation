db18ae84d99d6172417e0f34ba9925c3e51dcab5
abhishek pandey <abhishekpy55@gmail.com>
2024-06-20 11:07:43 +0530

    java program

---
+++ src/JUN1/BX6_18_Jun_RestAssured/NS.java
@@ -0,0 +1,21 @@
+package JUN1.BX6_18_Jun_RestAssured;
+
+public class NS {
+    void step1(){
+        System.out.println("Step_1");
+    }
+    void step2(){
+        System.out.println("Step_2");
+    }
+    void step3(){
+        System.out.println("Step_3");
+    }
+
+    public static void main(String[] args) {
+        NS ns = new NS();
+        ns.step1();
+        ns.step2();
+        ns.step3();
+
+    }
+}
